891e7a811f938cb042f699bc12ac959ddb21a3ae
locvo <locvo.7401@gmail.com>
2023-09-21 15:01:51 +0700

    Header

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,5 +1,36 @@
+import styles from './Header.module.scss'
+import classNames from 'classnames/bind'
+import images from '~/assets/images'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+
+const cx = classNames.bind(styles)
 function Header() {
-    return (<h2>Header</h2>);
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}>
+                <div className={cx('logo')}>
+                    <img src={images.logo} alt='tiktok'></img>
+                </div>
+
+                <div className={cx('search')}>
+                    <input placeholder='Search accounts and videos' spellCheck='false' />
+                    <button className={cx('close-btn')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                    <FontAwesomeIcon icon={faSpinner} className={cx('loading')} />
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+
+                <div className={cx('action')} >
+
+                </div>
+            </div>
+        </header>
+    )
+
 }
 
 export default Header;
